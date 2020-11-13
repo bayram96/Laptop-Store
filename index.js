58d179d53fdfda2ced4154991fa732d9b2b88c10
Bayramgeldi <bnurberdiyev@colotek.com>
2020-11-13 10:24:23 -0500

    added main and laptop details page

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const card = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server!');
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    console.log(id);
+    // PRODUCTS OVERVIEW
+    if (pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let allCards = laptopData.map((laptop) => {
+                return replaceTemplate(card, laptop);
+                //allCards = allCards + cards;
+            }).join('');
+            console.log(allCards);
+            let output = data.replace(/{%CARDS%}/g, allCards);
+            
+            res.end(output);
+
+        });
+    // LAPTOP DETAILS
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop)
+            res.end(output);
+
+        });
+    // PAGE NOT FOUND
+    } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, {'Content-type': 'image/jpg'});
+            res.end(data);
+        })
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html'});
+        res.end('Page not found');
+    }
+    
 })
 
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now ')
 })
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
